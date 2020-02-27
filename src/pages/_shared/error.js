34ad6014e34abb044bf55e798d5936b7ad87846b
Chan Khang-Woei <chan.khang-woei@reedonline.co.uk>
2020-02-27 17:10:53 +0000

    added some pages and page routing

---
+++ src/pages/_shared/error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h3> Error Not Found </h3>
+            <small> Error : 404 </small>
+        </div>
+    );
+}
+
+export default NotFoundPage;
---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div>
+            <h3> Home Page </h3>
+        </div>
+    );
+}
+
+
+export default HomePage;
