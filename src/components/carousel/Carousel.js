979d2ad3f95245574436bab951edfffb10534dc4
kwc43 <ckhangwoei@gmail.com>
2020-03-01 15:34:05 +0000

    added a toggle on the 'Sign Up Today' button

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,19 +1,35 @@
 import React,  { Component } from 'react';
 import './Carousel.css';
+import Modal from "../modal/Modal.js";
 
 export default class Header extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggleLogInModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div class="carousel">
         <div class="carousel-inner">
           <div class="item active carousel-image-1">
             <div class="carousel-caption">
-              <h1> B1-1F </h1>
+              <h1> B11F </h1>
               <p> Ensuring fair price for all </p>
               <p>
-                <a class="btn btn-lg btn-primary">
+                <button class="btn btn-lg btn-primary" onClick={this.toggleLogInModal}>
                   Sign Up Today
-                </a>
+                </button>
               </p>
             </div>
           </div>
