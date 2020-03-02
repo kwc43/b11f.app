add0394b8099a0c1a6c5ce1faf6198d554be89de
Chan Khang-Woei <chan.khang-woei@reedonline.co.uk>
2020-03-02 16:48:03 +0000

    migrating to bootstrap 4

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,29 +7,17 @@ import './Header.css';
 export default class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <Link className="navbar-brand" to="/">
-              B11F
-            </Link>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/"> B11F </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse navbar-right" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link className="nav-item nav-link active" to="/dashboard"> Your Dashboard </Link>
+            <Link className="nav-item nav-link active" to="/"> About Us </Link>
+            <Link className="nav-item nav-link active" to="/"> Contact Us </Link>
           </div>
-              <ul className="nav navbar-nav navbar-right">
-                 <li>
-                  <Link to="/dashboard">
-                    Your Dashboard
-                  </Link>
-                </li>
-                <li>
-                 <Link to="/">
-                   About Us
-                 </Link>
-               </li><li>
-                <Link to="/">
-                  Contact Us
-                </Link>
-              </li>
-               </ul>
         </div>
       </nav>
     )
